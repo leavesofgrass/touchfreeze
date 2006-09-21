abcbbe334ee98cfb8671f87cbcdd89d03280763c
chemodax <chemodax@37361e48-f61d-0410-aa1e-e186e184c3e4>
2006-09-21 13:12:13 +0000

    Initial import.

---
+++ Sources/TestApp/KeyHookTest.cpp
@@ -0,0 +1,240 @@
+#include "stdafx.h"
+
+#include "..\HookDll\HookDll.h"
+#include "AboutDlg.h"
+#include "resource.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+const UINT     wm_KBHookNotify = RegisterWindowMessage( TFHookNotifyMsg );
+const UINT     wm_ShellNotify = WM_APP + 1;
+NOTIFYICONDATA m_NotifyIcon;
+HICON          g_hIcon;
+
+HINSTANCE      g_hInst = NULL;
+
+static LPCTSTR WINDOW_CLASS_NAME = _T("TouchFreezeWindowClass"); 
+static LPCTSTR WINDOW_NAME       = _T("{1087E1B6-7C1F-49d6-8476-15E6CD8B4266}"); 
+static LPCTSTR TOUCHFREEZE_KEY   = _T("TouchFreeze");
+static LPCTSTR AUTORUN_KEY       = _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+static LPCTSTR DONATE_URL        = _T("http://www.yaranga.net/TouchFreeze/#donate");
+static LPCTSTR WEBSITE_URL       = _T("http://www.yaranga.net/TouchFreeze/");
+
+static LONG RegSetStringValue(HKEY hKey, LPCTSTR valueName, LPCTSTR value)
+{
+    return ::RegSetValueEx(hKey, valueName, 0, REG_SZ,
+        (const BYTE *) value, _tcslen(value)*sizeof(TCHAR)); 
+}
+
+static void OpenURL(HWND hwnd, LPCTSTR url)
+{
+    ShellExecute(hwnd, NULL, url, NULL, NULL, SW_SHOWNORMAL);
+}
+
+void ContactOrDonate(HWND hwnd, int type)
+{
+    switch(type)
+    {
+    case 1: OpenURL(hwnd, DONATE_URL); break;
+    case 2: OpenURL(hwnd, WEBSITE_URL);  break;
+    }
+}
+
+static void SetAutorun(BOOL autoRun)
+{
+    HKEY  regKey;
+    TCHAR moduleFileName[_MAX_PATH];
+            
+    GetModuleFileName(g_hInst, moduleFileName, _MAX_PATH);
+
+    // Create provider key
+    if (RegCreateKey(HKEY_CURRENT_USER, AUTORUN_KEY, &regKey) 
+        != ERROR_SUCCESS)
+    {
+        return ;
+    }
+
+    if (autoRun)
+        RegSetStringValue(regKey, TOUCHFREEZE_KEY, moduleFileName);
+    else
+        RegDeleteValue(regKey, TOUCHFREEZE_KEY);
+
+    RegCloseKey(regKey);
+}
+
+static BOOL IsAutorun()
+{
+    HKEY  regKey;
+    TCHAR moduleFileName[_MAX_PATH];
+    TCHAR regFileName   [_MAX_PATH];
+            
+    GetModuleFileName(g_hInst, moduleFileName, _MAX_PATH);
+
+    // Create provider key
+    if (RegOpenKeyEx(HKEY_CURRENT_USER, AUTORUN_KEY, 0, KEY_ALL_ACCESS, &regKey) 
+        != ERROR_SUCCESS)
+    {
+        return FALSE;
+    }
+
+    DWORD size = sizeof(regFileName);
+    
+    if (RegQueryValueEx(regKey, TOUCHFREEZE_KEY, NULL, NULL, (LPBYTE) regFileName, &size) 
+        != ERROR_SUCCESS)
+    {
+        regFileName[0] = 0;
+    }
+    
+    RegCloseKey(regKey);
+    return _tcsicmp(regFileName, moduleFileName) == 0;
+}
+
+static void ShowContextMenu(HWND hwnd)
+{
+    HMENU hMenu = LoadMenu(g_hInst, MAKEINTRESOURCE(IDR_MENU));
+    HMENU hMenuPopup = GetSubMenu(hMenu, 0);
+    
+    if (IsAutorun())
+        DeleteMenu(hMenu, ID_AUTOSTART_ON, MF_BYCOMMAND);
+    else
+        DeleteMenu(hMenu, ID_AUTOSTART_OFF, MF_BYCOMMAND);
+
+    POINT pt;
+    GetCursorPos(&pt);
+ 
+    SetForegroundWindow(hwnd);
+
+    TrackPopupMenu(hMenuPopup, TPM_LEFTBUTTON|TPM_LEFTALIGN,
+        pt.x, pt.y, 0, hwnd, NULL);
+
+    PostMessage(hwnd, WM_NULL, 0, 0);
+
+    DestroyMenu(hMenu);
+}
+
+LRESULT CALLBACK MainWindowProc(
+   HWND hWnd,
+   UINT uMsg,
+   WPARAM wParam,
+   LPARAM lParam 
+)
+{
+    switch(uMsg)
+    {
+    case WM_CREATE:
+        ZeroMemory(&m_NotifyIcon, sizeof(m_NotifyIcon));
+        m_NotifyIcon.cbSize = sizeof(m_NotifyIcon);
+        m_NotifyIcon.hIcon = g_hIcon;
+        m_NotifyIcon.hWnd = hWnd;
+        m_NotifyIcon.uFlags = NIF_ICON|NIF_MESSAGE|NIF_TIP;
+        m_NotifyIcon.uCallbackMessage = wm_ShellNotify;
+        m_NotifyIcon.uID = 1;                
+        _tcscpy(m_NotifyIcon.szTip, _T("TouchFreeze (Automatic mode)"));
+        Shell_NotifyIcon(NIM_ADD, &m_NotifyIcon);
+        return 0;
+
+    case WM_DESTROY:
+        Shell_NotifyIcon(NIM_DELETE, &m_NotifyIcon);
+        return 0;
+ 
+    case WM_COMMAND:
+        switch(LOWORD(wParam))
+        {
+        case ID_ABOUT:
+            ShowAboutDlg(g_hInst, hWnd);
+            break;
+        
+        case ID_EXIT:
+            PostQuitMessage(1);
+            break;
+        
+        case ID_AUTOSTART_ON:
+            SetAutorun(TRUE);
+            break;
+        
+        case ID_AUTOSTART_OFF:
+            SetAutorun(FALSE);
+            break;
+
+        case ID_DONATE:
+            ContactOrDonate(hWnd, 1);
+            break;
+        }
+        return 0;
+
+    case WM_CLOSE:
+        PostQuitMessage(1);
+        return 0;
+    }
+
+    if (uMsg == wm_KBHookNotify)
+    {
+        return 0;
+    }
+    else if (uMsg == wm_ShellNotify)
+    {
+        switch(lParam)
+        {
+        case WM_LBUTTONUP:
+        case WM_RBUTTONUP:
+            ShowContextMenu(hWnd);
+            break;
+        }
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR lpCmdLine, int nCmdShow)
+{
+    g_hInst = hInst;
+
+    HWND hPrevHWND = FindWindow(WINDOW_CLASS_NAME, WINDOW_NAME);
+
+    if (hPrevHWND)
+        PostMessage(hPrevHWND, WM_CLOSE, 0, 0);
+
+    if(_tcsstr(lpCmdLine, _T("unload")) != 0)
+    {
+        return 0;
+    }
+
+
+    WNDCLASSEX wndClass;
+    ZeroMemory(&wndClass, sizeof(wndClass));
+    wndClass.cbSize        = sizeof(wndClass);
+    wndClass.hInstance     = hInst;
+    wndClass.lpfnWndProc   = MainWindowProc;
+    wndClass.lpszClassName = WINDOW_CLASS_NAME;
+    if (!RegisterClassEx(&wndClass))
+        return -1;
+
+    g_hIcon = (HICON) ::LoadImage(hInst, MAKEINTRESOURCE(IDR_MAINFRAME),
+        IMAGE_ICON, 16, 16, 0);
+
+    HWND hwnd = CreateWindow(WINDOW_CLASS_NAME, WINDOW_NAME, 
+        WS_OVERLAPPED, 0,0,0,0, HWND_MESSAGE, NULL, hInst, NULL);
+
+    if (!hwnd)
+        return -2;
+
+    TFHookInstall(hwnd);	
+
+    MSG msg;
+    while( GetMessage( &msg, NULL, 0, 0 ))
+    { 
+        TranslateMessage(&msg); 
+        DispatchMessage(&msg); 
+    }
+    
+    TFHookUninstall();
+    
+    DestroyWindow(hwnd);
+    UnregisterClass(WINDOW_CLASS_NAME, hInst);
+    return 0;
+}
---
+++ Sources/TestApp/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by KeyHookTest.rc
+//
+#define IDR_MAINFRAME                   128
+#define IDR_MENU                        129
+#define IDD_ABOUT                       130
+#define IDC_LIST1                       1000
+#define IDC_EXIT                        1001
+#define IDC_EDIT1                       1002
+#define IDC_EMAIL                       1004
+#define IDC_DONATE_AMAZON               1005
+#define ID_ABOUT                        32774
+#define ID_EXIT                         32776
+#define ID_AUTOSTART_OFF                32778
+#define ID_AUTOSTART_ON                 32779
+#define ID__DONATE                      32780
+#define ID_DONATE                       32782
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
