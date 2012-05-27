863b92a7a33aaec21f7812a34be7f7c4198fc90b
chemodax <chemodax@37361e48-f61d-0410-aa1e-e186e184c3e4>
2012-05-27 17:43:33 +0000

    Navigater on main page for donation.

--- trunk/Sources/TestApp/KeyHookTest.cpp
+++ trunk/Sources/TestApp/KeyHookTest.cpp
@@ -21,7 +21,7 @@ static LPCTSTR WINDOW_CLASS_NAME = _T("TouchFreezeWindowClass");
 static LPCTSTR WINDOW_NAME       = _T("{1087E1B6-7C1F-49d6-8476-15E6CD8B4266}"); 
 static LPCTSTR TOUCHFREEZE_KEY   = _T("TouchFreeze");
 static LPCTSTR AUTORUN_KEY       = _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
-static LPCTSTR DONATE_URL        = _T("https://order.kagi.com/?6FBAU");
+static LPCTSTR DONATE_URL        = _T("http://touchfreeze.googlecode.com/");
 static LPCTSTR WEBSITE_URL       = _T("http://touchfreeze.googlecode.com/");
 
 static LONG RegSetStringValue(HKEY hKey, LPCTSTR valueName, LPCTSTR value)
