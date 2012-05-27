305031b538f338cab96f719a360ec76c8f5b7433
chemodax <chemodax@37361e48-f61d-0410-aa1e-e186e184c3e4>
2012-05-27 17:59:09 +0000

    Implement donate button in installation package.

---
+++ Sources/TestApp/Constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+static LPCTSTR WINDOW_CLASS_NAME = _T("TouchFreezeWindowClass"); 
+static LPCTSTR WINDOW_NAME       = _T("{1087E1B6-7C1F-49d6-8476-15E6CD8B4266}"); 
+static LPCTSTR TOUCHFREEZE_KEY   = _T("TouchFreeze");
+static LPCTSTR AUTORUN_KEY       = _T("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+static LPCTSTR DONATE_URL        = _T("http://touchfreeze.googlecode.com/");
+static LPCTSTR WEBSITE_URL       = _T("http://touchfreeze.googlecode.com/");
